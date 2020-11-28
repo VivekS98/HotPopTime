6d10260034ac435174db405486089ee5f6a17db3
Vivek Kashyap <vivekyeskashyap@gmail.com>
2020-11-28 16:07:50 +0530

    improvements

--- src/components/movie-show.js
+++ src/components/movie-show.js
@@ -25,18 +25,25 @@ export default function MovieShow() {
                     <img src={`https://image.tmdb.org/t/p/original${data.backdrop_path}`} className="movie-backdrop" alt="backdrop" />
                     <div className="movie-info d-flex flex-row">
                         <img src={`https://image.tmdb.org/t/p/original${data.poster_path}`} className="movie-poster-details" alt="poster" />
-                        <div className="d-flex flex-column">
-                            <h2 className="text-light">{data.title ? data.title : data.name}</h2>
-                            <h5 className="text-white-50">{data.release_date ? data.release_date : data.first_air_date}</h5>
+                        <div className="d-flex flex-column justify-content-around">
+                            <div className="d-flex flex-column">
+                                <h2 className="text-light">{data.title ? data.title : data.name}</h2>
+                                <h6 className="movie-title text-white-50">{data.tagline}</h6>
+                                <h5 className="movie-title text-white-50">{data.release_date ? data.release_date : data.first_air_date}</h5>
+                            </div>
                             <div className="d-flex flex-row flex-wrap">
-                            <h4 className="text-light">Genres:</h4>
-                            {genresView}
+                                <h4 className="text-white">Genres:</h4>
+                                {genresView}
+                            </div>
+                            <div className="d-flex flex-row flex-wrap">
+                                <h4 className="text-white">Language:</h4>
+                                <h6 className="text-white-50 movie-title">{data.spoken_languages[0].english_name}</h6>
                             </div>
                         </div>
                     </div>
                     <div className="movie-overview">
-                        <h3 className="text-light">Overview</h3>
-                        <p className="text-white">{data.overview}</p>
+                        <h3 className="text-light">Overview:</h3>
+                        <p className="movie-title text-white">{data.overview}</p>
                     </div>
                 </div>
             );
