b63d0d9d51f81445b5751e329bf6cb155ab349f9
Vivek Kashyap <vivekyeskashyap@gmail.com>
2020-12-01 14:31:37 +0530

    bugs fixed

--- src/components/movie-show.js
+++ src/components/movie-show.js
@@ -61,42 +61,42 @@ export default function MovieShow() {
             return (
                 <FixedDiv>
                     <div className="movie-details w-100 h-100">
-                    <div className="movie-info d-flex flex-row">
-                        <img src={`https://image.tmdb.org/t/p/w300${data.poster_path}`} className="movie-poster-details" alt="poster" />
-                        <div className="d-flex flex-column justify-content-around">
-                            <div className="d-flex flex-column">
-                                <h3 className="text-light">{data.title ? data.title : data.name}</h3>
-                                <h6 className="movie-title text-white-50">{data.tagline}</h6>
-                                <h5 className="movie-title text-white-50">{data.release_date ? data.release_date : data.first_air_date}</h5>
-                            </div>
-                            <div className="d-flex flex-row flex-wrap">
-                                <h4 className="text-white">Adult:</h4>
-                                <h6 className="text-white-50 movie-title">{data.adult ? 'True' : 'False'}</h6>
-                            </div>
-                            <div className="d-flex flex-row flex-wrap">
-                                <h4 className="text-white">Language:</h4>
-                                <h6 className="text-white-50 movie-title">{data.spoken_languages[0] ? data.spoken_languages[0].english_name : 'unknown'}</h6>
+                        <div className="movie-info d-flex flex-row">
+                            <img src={`https://image.tmdb.org/t/p/w300${data.poster_path}`} className="movie-poster-details" alt="poster" />
+                            <div className="d-flex flex-column justify-content-around">
+                                <div className="d-flex flex-column">
+                                    <h3 className="text-light">{data.title ? data.title : data.name}</h3>
+                                    <h6 className="movie-title text-white-50">{data.tagline}</h6>
+                                    <h5 className="movie-title text-white-50">{data.release_date ? data.release_date : data.first_air_date}</h5>
+                                </div>
+                                <div className="d-flex flex-row flex-wrap">
+                                    <h4 className="text-white">Adult:</h4>
+                                    <h6 className="text-white-50 movie-title">{data.adult ? 'True' : 'False'}</h6>
+                                </div>
+                                <div className="d-flex flex-row flex-wrap">
+                                    <h4 className="text-white">Language:</h4>
+                                    <h6 className="text-white-50 movie-title">{data.spoken_languages[0] ? data.spoken_languages[0].english_name : 'unknown'}</h6>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="movie-overview d-flex flex-row flex-wrap">
-                        <h4 className="text-white">Genres:</h4>
-                        {genresView}
-                    </div>
-                    <div className="movie-overview">
-                        <h3 className="text-white">Overview:</h3>
-                        <p className="movie-title text-justify text-white">{data.overview}</p>
-                    </div>
-                    <div className="movie-overview">
-                        <h3 className="text-white">Production:</h3>
-                        <Production data={data.production_companies} />
-                    </div>
-                    <div className="movie-overview">
-                        <h2 className="text-white ml-3">Similar</h2>
-                        <div className="movie-list d-flex flex-row overflow-auto">
-                        {similarView}
+                        <div className="movie-overview d-flex flex-row flex-wrap">
+                            <h4 className="text-white">Genres:</h4>
+                            {genresView}
                         </div>
-                    </div>
+                        <div className="movie-overview">
+                            <h3 className="text-white">Overview:</h3>
+                            <p className="movie-title text-justify text-white">{data.overview}</p>
+                        </div>
+                        <div className="movie-overview">
+                            <h3 className="text-white">Production:</h3>
+                            <Production data={data.production_companies} />
+                        </div>
+                        <div className="movie-overview">
+                            <h2 className="text-white ml-3">Similar</h2>
+                            <div className="movie-list d-flex flex-row overflow-auto">
+                            {similarView}
+                            </div>
+                        </div>
                     </div>
                 </FixedDiv>
             );
