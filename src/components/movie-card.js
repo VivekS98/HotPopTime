cfc6f1e928ed08f8f1d595a6e0ed0bc79cdb37a9
Vivek Kashyap <vivekyeskashyap@gmail.com>
2020-12-02 14:05:27 +0530

    some improvements for mobile

--- src/components/movie-card.js
+++ src/components/movie-card.js
@@ -13,8 +13,8 @@ export default function MovieCard({type, movie}){
         <div className="movie-card" onClick={() => handleClick()}>
             <img src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`} className="movie-poster" alt="poster" />
             <div className="card-body">
-                <h5 className="text-white">{movie.title ? movie.title : movie.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{movie.release_date ? movie.release_date : movie.first_air_date}</h6>
+                <h5 className="margin-title text-white">{movie.title ? movie.title : movie.name}</h5>
+                <h6 className="margin-title card-subtitle mb-2 text-muted">{movie.release_date ? movie.release_date : movie.first_air_date}</h6>
             </div>
         </div>
     );
