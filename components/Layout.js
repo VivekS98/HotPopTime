e924f8b6d21644efa4e4682ec1f6a86592071df9
Vivek S <vivekyeskashyap@gmail.com>
2023-08-28 13:11:48 +0530

    Update Layout.js
    Changed VKcodes url

--- components/Layout.js
+++ components/Layout.js
@@ -59,7 +59,7 @@ export default function Layout({ children }) {
           </h3>
           <a
             className="text-lg font-semibold transform-gpu duration-200 text-[gold] md:text-xl hover:text-yellow-400"
-            href="https://vkcodes.vercel.app"
+            href="https://vkcodes.com"
             rel="noreferrer"
             target="_blank"
           >
